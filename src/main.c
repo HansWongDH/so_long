7304ee4844f9495993f2745d1e6ae55358c4f4c2
Hanswong <hansdhwong@gmail.com>
2022-04-06 01:46:12 +0800

    stupid mistake

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: wding-ha <wding-ha@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 14:10:12 by wding-ha          #+#    #+#             */
-/*   Updated: 2022/04/04 19:31:09 by wding-ha         ###   ########.fr       */
+/*   Updated: 2022/04/06 01:45:40 by wding-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@ int	main(int argc, char **argv)
 	t_map	m;
 	t_info	info;
 
+	if (argc != 2)
+		return (0);
 	initialize_map(&m);
 	initialize_info(&info);
-	if (argc > 1)
-	{
-		info.map = start_map(argv, &m);
-		if (!info.map)
-			return (0);
-	}
+	info.map = start_map(argv, &m);
+	if (!info.map)
+		return (0);
 	if (!map_validation(info.map, m))
 	{
 		error_message(info.map);
@@ -34,4 +33,5 @@ int	main(int argc, char **argv)
 	info.win = mlx_new_window(info.mlx, m.length * 32, m.height * 32, "Game");
 	game_init(&info);
 	mlx_loop(info.mlx);
+	return (0);
 }
